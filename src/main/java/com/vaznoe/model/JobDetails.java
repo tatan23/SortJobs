8b6b478373c0d7580e3e70ce66823e2b71a3fd74
vaznoe <vsp015@gmail.com>
2019-05-23 02:22:38 -0700

    added project

---
+++ src/main/java/com/vaznoe/model/JobDetails.java
@@ -0,0 +1,26 @@
+package com.vaznoe.model;
+
+public class JobDetails {
+
+    public String department;
+    public String firstName;
+    public String lastName;
+    public String jobTitle;
+    public double hourlyRate;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+}
---
+++ src/main/java/com/vaznoe/model/JobOutput.java
@@ -0,0 +1,12 @@
+package com.vaznoe.model;
+
+public class JobOutput {
+
+    public String department;
+    public String jobTitle;
+    public double averageHourlyRate;
+
+    public double getAverageHourlyRate() {
+        return this.averageHourlyRate;
+    }
+}
